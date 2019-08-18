c72184acfb926a557aa094645dd69a98ce3879e6
Florin Galea <floringalea@gmail.com>
2019-08-18 21:07:01 +0100

    Cleaned config.php

--- App/config.php
+++ App/config.php
@@ -1,6 +1,4 @@
 <?php
-<<<<<<< HEAD
-
 /*
 | DATABASE CONFIG
 */
@@ -16,9 +14,6 @@ $db = array(
 | Define db connection
 */
 $conn = mysqli_connect($db['hostname'], $db['username'], $db['password']);
-=======
-$conn = mysqli_connect('localhost','root', '');
->>>>>>> 43ba814043711368a5a1eccccbdb16eea1678fd3
 
 if (!$conn){
 
@@ -26,21 +21,14 @@ if (!$conn){
 
 }
 
-<<<<<<< HEAD
 $select_db = mysqli_select_db($conn, $db['database']);
-=======
-$select_db = mysqli_select_db($conn, 'school_s');
->>>>>>> 43ba814043711368a5a1eccccbdb16eea1678fd3
 
 if (!$select_db){
 
     die("Database Selection Failed" . mysqli_connect_error());
 
 }
-<<<<<<< HEAD
 
 
-=======
 session_start();
  ?>
->>>>>>> 43ba814043711368a5a1eccccbdb16eea1678fd3
